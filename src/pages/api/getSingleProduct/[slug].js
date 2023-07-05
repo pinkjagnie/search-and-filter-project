c31f200724870bdd41ae4677c48fe73978332feb
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-05 22:35:00 +0200

    Some clean up after struggling with that typo

--- src/pages/api/getSingleProduct/[slug].js
+++ src/pages/api/getSingleProduct/[slug].js
@@ -10,21 +10,7 @@ export default async function handler(req, res) {
 
   let slug_to_find = slug;
 
-  // let str = "This-is-a-news-item-";
   let title = slug_to_find.replace(/-/g, " ");
-
-  // const { slug } = req.body;
-
-  console.log("slag z apiiii " + slug_to_find);
-  console.log("tajtlyyy z api " + title);
-
-  // let title = slug.replace(/-/g, " ");
-
-  // {/article/${article.title.replace(/ /g, "-")}}
-
-  // const { title } = req.body;
-
-  // console.log("tajtle " + title);
 
   await connectToDatabase();
 
