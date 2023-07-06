a250c3289abdd40e5a2dd69df3d3a2fdaee1d794
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-06 12:35:25 +0200

    Merged projects - including search bar comp.

---
+++ src/pages/search/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import SearchBar from "@/components/SearchBar";
+
+const searchPage = () => {
+  return (
+    <section className="pt-28 min-h-screen">
+      <h1 className="text-center text-2xl font-bold">To search enter below</h1>
+      <SearchBar />
+    </section>
+  );
+};
+
+export default searchPage;
