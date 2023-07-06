167368d2c10315fa28aaef51353c02b5c9087bf9
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-06 20:11:09 +0200

    Finally filtering searched products

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import axios from "axios";
+
+import SingleProduct from "./SingleProduct";
 
 import { SlMagnifier } from "react-icons/sl";
 
-const SearchBar = () => {
-  const [search, setSearch] = useState("");
+const SearchBar = ({ products }) => {
+  const searchRef = useRef();
+  const [filteredData, setFilteredData] = useState([]);
 
   const searchHandler = (event) => {
     event.preventDefault();
-    console.log(search);
-    setSearch("");
+
+    let query = searchRef.current.value;
+    console.log(query);
+
+    searchFilteredProducts(query);
+  };
+
+  const searchFilteredProducts = (query) => {
+    let url = `/api/searchforproducts/${query}`;
+    console.log("url " + url);
+
+    axios.get(url).then(
+      (response) => {
+        console.log(response);
+        setFilteredData(response.data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   };
 
   return (
-    <div>
-      {/* Search bar */}
-      <div className="w-[80%] h-[25vh] mt-10 mx-auto rounded-md bg-gray-600 flex items-center justify-center">
-        <input
-          type="search"
-          name="search"
-          placeholder="Search"
-          value={search}
-          className="bg-gray-100 text-gray-700 w-[80%] h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-          onChange={(event) => {
-            setSearch(event.target.value);
-          }}
-        />
-        <button type="submit" className="-ml-12" onClick={searchHandler}>
-          <SlMagnifier size={30} className="text-gray-600" />
-        </button>
+    <>
+      <div>
+        {/* Search bar */}
+        <div className="w-[80%] h-[25vh] mt-10 mx-auto rounded-md bg-gray-600 flex items-center justify-center">
+          <input
+            type="text"
+            name="search"
+            placeholder="Search"
+            ref={searchRef}
+            className="bg-gray-100 text-gray-700 w-[80%] h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          />
+          <button type="submit" className="-ml-12" onClick={searchHandler}>
+            <SlMagnifier size={30} className="text-gray-600" />
+          </button>
+        </div>
       </div>
-    </div>
+      {filteredData &&
+        filteredData.map((product) => {
+          return <SingleProduct key={product.id} product={product} />;
+        })}
+    </>
   );
 };
 
---
+++ src/pages/api/searchforproducts/[query].js
@@ -0,0 +1,22 @@
+import connectToDatabase from "../../../../lib/db";
+import Handmade from "../../../../models/Handmade";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return;
+  }
+
+  const { query } = req.query;
+
+  let query_to_find = query;
+
+  await connectToDatabase();
+
+  const products = await Handmade.find({
+    $or: [{ title: query_to_find }, { description: query_to_find }],
+  }).exec();
+
+  console.log("produkty z api " + products);
+
+  res.status(201).json(products);
+}
